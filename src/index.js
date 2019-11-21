c43b859acfc73bffab8eb156e7de6fd36f440736
tangguixiang <1126531749@qq.com>
2019-11-21 17:22:13 +0800

    cesium与webpack环境搭建

---
+++ src/index.js
@@ -0,0 +1,5 @@
+var Cesium = require('cesium/Cesium');
+require('cesium/Widgets/widgets.css');
+require('./css/main.css');
+var viewer = new Cesium.Viewer('cesiumContainer');
+
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const CopywebpackPlugin = require('copy-webpack-plugin');
+// Cesium源码所在目录
+const cesiumSource = 'node_modules/cesium/Source';
+const cesiumWorkers = '../Build/Cesium/Workers';
+
+
+module.exports = {
+	devtool: 'eval',//不推荐在最终产品代码里添加Source maps（容易被破解呗）
+    context: __dirname,
+    entry: {
+        app: './src/index.js'
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+        //需要编译Cesium中的多行字符串 
+        sourcePrefix: ''
+    },
+    amd: {
+        //允许Cesium兼容 webpack的require方式 
+        toUrlUndefined: true
+    },
+    node: {
+        // 解决fs模块的问题（Resolve node module use of fs）
+        fs: 'empty'
+    },
+    module: {
+        
+        rules: [{
+            test: /\.css$/,
+            use: [ 'style-loader', 'css-loader' ]
+        }, {
+            test: /\.(png|gif|jpg|jpeg|svg|xml|json)$/,
+            use: [ 'url-loader' ]
+        }],
+            // 解决：Critical dependency: require function is used in a way in which dependencies cannot be statically extracted
+    unknownContextCritical: false,
+    },
+    plugins: [
+            // 解决：Unable to determine Cesium base URL automatically,…efining a global variable called CESIUM_BASE_URL.
+    new webpack.DefinePlugin({
+        // Define relative base path in cesium for loading assets
+        CESIUM_BASE_URL: JSON.stringify(''),
+      }),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+        // 拷贝Cesium 资源、控价、web worker到静态目录 
+        new CopywebpackPlugin([ { from: path.join(cesiumSource, cesiumWorkers), to: 'Workers' } ]),
+        new CopywebpackPlugin([ { from: path.join(cesiumSource, 'Assets'), to: 'Assets' } ]),
+        new CopywebpackPlugin([ { from: path.join(cesiumSource, 'Widgets'), to: 'Widgets' } ])
+    ],
+	    // 开发服务器配置
+    devServer: {
+        contentBase: path.join(__dirname, "dist")
+    },
+    
+	resolve: {
+        alias: {
+            // Cesium模块名称
+            cesium: path.resolve(__dirname, cesiumSource)
+        }
+    },
+	
+};
