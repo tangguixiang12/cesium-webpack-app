aafcd4a815d00661e442080e19e14bce9b8f6959
tangguixiang <1126531749@qq.com>
2019-11-22 17:56:15 +0800

    cesium 结合 vue

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-var Cesium = require('cesium/Cesium');
-require('cesium/Widgets/widgets.css');
-require('./css/main.css');
-var viewer = new Cesium.Viewer('cesiumContainer');
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App.vue'
 
+Vue.config.productionTip = false
+
+
+
+/* eslint-disable no-new */
+new Vue({
+  render: h => h(App)
+}).$mount("#app")
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const CopywebpackPlugin = require('copy-webpack-plugin');
 // Cesium源码所在目录
 const cesiumSource = 'node_modules/cesium/Source';
@@ -10,6 +10,7 @@ const cesiumWorkers = '../Build/Cesium/Workers';
 
 
 module.exports = {
+    stats: { children: false },//解决html-webpack-plugin Entrypoint undefined = index.html
 	devtool: 'eval',//不推荐在最终产品代码里添加Source maps（容易被破解呗）
     context: __dirname,
     entry: {
@@ -31,25 +32,41 @@ module.exports = {
     },
     module: {
         
-        rules: [{
-            test: /\.css$/,
-            use: [ 'style-loader', 'css-loader' ]
-        }, {
+        rules: [
+            {
+                test: /\.scss$/,
+                loaders: ['style-loader', 'css-loader', 'sass-loader']
+                },
+                {
+                    test: /\.css$/,
+                    loaders: ['style-loader', 'css-loader', 'sass-loader']
+                    },
+            
+        {
             test: /\.(png|gif|jpg|jpeg|svg|xml|json)$/,
             use: [ 'url-loader' ]
-        }],
+        },      {
+            test: /\.vue$/,
+            loader: 'vue-loader'
+          }
+    ],
             // 解决：Critical dependency: require function is used in a way in which dependencies cannot be statically extracted
     unknownContextCritical: false,
     },
     plugins: [
+        new VueLoaderPlugin(),
             // 解决：Unable to determine Cesium base URL automatically,…efining a global variable called CESIUM_BASE_URL.
-    new webpack.DefinePlugin({
+    new webpack.DefinePlugin(
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        })),
+        //解决CESIUM_BASE_URL.错误
+      new webpack.DefinePlugin(
+      {
         // Define relative base path in cesium for loading assets
         CESIUM_BASE_URL: JSON.stringify(''),
       }),
-        new HtmlWebpackPlugin({
-            template: 'src/index.html'
-        }),
+
         // 拷贝Cesium 资源、控价、web worker到静态目录 
         new CopywebpackPlugin([ { from: path.join(cesiumSource, cesiumWorkers), to: 'Workers' } ]),
         new CopywebpackPlugin([ { from: path.join(cesiumSource, 'Assets'), to: 'Assets' } ]),
