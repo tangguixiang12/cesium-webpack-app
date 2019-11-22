2b7119a3b205c0dab2eb25f73eb98599f53984ba
tangguixiang <1126531749@qq.com>
2019-11-22 17:56:56 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <Cesium/>
+  </div>
+</template>
+
+<script>
+ import Cesium from './components/Cesium.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Cesium
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+}
+</style>
---
+++ src/components/Cesium.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="cesiumContainer">
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <HelloWorld/> -->
+  </div>
+</template>
+
+<script>
+
+import * as Cesium from 'cesium/Cesium'
+import 'cesium/Widgets/widgets.css'
+export default {
+  name: 'Cesium',
+  // components: {
+  //   HelloWorld
+  // },
+  mounted(){
+
+
+var viewer = new Cesium.Viewer('cesiumContainer');
+  }
+}
+</script>
+
+<style lang="scss">
+
+@import '../css/main.scss';
+</style>
